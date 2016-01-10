a577701d0abe946b1cdb86994accd864f909d361
Kat <kat@polypod.me>
2016-01-10 18:47:33 +0000

    Added redbean & composer-based libraries

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
+require "settings.php";
+require "include/rb.php";
+require "vendor/autoload.php";
 
+session_start();
 
-?>
-
+R::setup('mysql:host='.$settings['database']['server'].'; dbname='.$settings['database']['database'],$settings['database']['username'],$settings['database']['password']);

63b5e8c5929034fc49b577b7ea9f4cef5a4198cb
Kat <kat@polypod.me>
2016-01-18 03:04:14 +0000

    Added example settings.

---
+++ settings.example.php
@@ -0,0 +1,9 @@
+<?php
+
+////// Parameters for connecting to the MySQL database. //////
+$settings['database']['password'] = "PASSWORD";
+$settings['database']['username'] = "root";
+$settings['database']['database'] = "declareit";
+$settings['database']['server'] = "localhost";
+
+$settings['site']['title'] = "Declare It!";
